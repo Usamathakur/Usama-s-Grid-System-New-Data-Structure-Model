418e5b34693cb22794dfbb0596aa3a5013ef3b96
Usamathakur <51357843+Usamathakur@users.noreply.github.com>
2020-12-01 11:26:51 +0530

    Amaa Function

---
+++ amaa.js
@@ -0,0 +1,11 @@
+var p;
+var n;
+var r;
+var set;
+
+function amaa(){
+    p = window.prompt("Enter your first number for the Grid: ");
+    n = window.prompt("Enter your second number: ");
+    r = p * p + n * n;
+    alert("Your answer is " + r);
+}
